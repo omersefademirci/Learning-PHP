ba7f2bed680b8049c905c90821be78737aa5f777
omersefademirci <omersefademirci@gmail.com>
2023-10-21 19:55:19 +0300

    PDO - Limit Order Where

---
+++ 19-PDO/limit-order-where.php
@@ -0,0 +1,29 @@
+<?php 
+
+try{
+    $db = new PDO('mysql:host=localhost;dbname=my_php','root');
+    
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // $db->setAttribute(3, 2);
+
+    $db->query('SET @@lc_time_names = "tr_TR"; ');
+    $db->query('SET CHARACTER SET utf8mb4');
+    $db->query('SET CHARACTER_SET_CONNECTION = utf8mb4');
+    
+
+}catch (PDOException $exception){
+    
+    echo $exception->getMessage();
+}
+echo "<pre>";
+
+$sql = "SELECT * FROM users WHERE user_name LIKE %Sefa%";
+
+$q = $db->query($sql);
+
+$users = $q->fetchAll(PDO::FETCH_ASSOC);
+print_r($users);
+
+
+
+?>
